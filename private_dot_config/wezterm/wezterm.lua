da30bf28e406eda0e84290fe81b9fd23f382e615
Dominik Garcia Bertapelle <d.bertapelle@hellogetsafe.com>
2023-03-30 00:15:14 +0200

    feat: switch colorscheme to kanagawa

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,14 +7,14 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
-config.font = wezterm.font 'Berkeley Mono'
+config.font = wezterm.font("Berkeley Mono")
 config.font_size = 13
 config.hide_tab_bar_if_only_one_tab = true
 
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = "Kanagawa (Gogh)"
 
 -- and finally, return the configuration to wezterm
 return config
