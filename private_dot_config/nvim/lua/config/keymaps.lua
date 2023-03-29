d085be042d8d919ccd37b862f8d7ca0bd2ba0d31
Dominik Garcia Bertapelle <d.bertapelle@hellogetsafe.com>
2023-03-30 00:17:33 +0200

    feat: add keymap to run a zellij pane with tests :chefkiss:

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,23 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+vim.keymap.set({ "n", "o", "x" }, "w", function()
+  require("spider").motion("w")
+end, { desc = "Spider-w" })
+vim.keymap.set({ "n", "o", "x" }, "e", function()
+  require("spider").motion("e")
+end, { desc = "Spider-e" })
+vim.keymap.set({ "n", "o", "x" }, "b", function()
+  require("spider").motion("b")
+end, { desc = "Spider-b" })
+vim.keymap.set({ "n", "o", "x" }, "ge", function()
+  require("spider").motion("ge")
+end, { desc = "Spider-ge" })
+map("n", "<leader>tz", ":!zellij run -f -- pnpm test %<cr>", { desc = "Run test in a Zellij floating pane" })
