4777b66726909ecbb2e38e1ebaa8ebe6a0b3831b
Dominik Garcia Bertapelle <d.bertapelle@hellogetsafe.com>
2023-03-30 00:15:51 +0200

    refactor: eslint for linting and prettier for formatting

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -49,15 +49,6 @@ return {
             },
           })
         end,
-        eslint = function()
-          require("lazyvim.util").on_attach(function(client)
-            if client.name == "eslint" then
-              client.server_capabilities.documentFormattingProvider = true
-            elseif client.name == "tsserver" then
-              client.server_capabilities.documentFormattingProvider = false
-            end
-          end)
-        end,
       },
     },
   },
