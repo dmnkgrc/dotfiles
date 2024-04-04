f1aff31db964ceda4ddb9346aefff7eb68563afc
Dominik Garcia Bertapelle <d.bertapelle@hellogetsafe.com>
2024-04-04 23:31:00 +0200

    feat: add better codeium and fugitive

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,10 +1,10 @@
 return {
-  {
-    "Exafunction/codeium.nvim",
-    opts = {
-      enable_chat = true,
-    },
-  },
+  -- {
+  --   "Exafunction/codeium.nvim",
+  --   opts = {
+  --     enable_chat = true,
+  --   },
+  -- },
   {
     enabled = false,
     "jackMort/ChatGPT.nvim",
@@ -29,6 +29,61 @@ return {
       { "<leader>ad", "<cmd>GpChatDelete<CR>", desc = "Delete last chat" },
       { "<leader>ap", "<cmd>GpChatPaste<CR>", mode = { "n", "v" }, desc = "Paste into last chat" },
       { "<leader>ar", "<cmd>GpRewrite<CR>", mode = { "n", "v" }, desc = "Rewrite using ChatGPT" },
+    },
+  },
+  {
+    "monkoose/neocodeium",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      {
+        "<A-f>",
+        function()
+          require("neocodeium").accept()
+        end,
+        mode = { "i" },
+        desc = "Accept codeium suggestion",
+      },
+      {
+        "<A-w>",
+        function()
+          require("neocodeium").accept_word()
+        end,
+        mode = { "i" },
+        desc = "Accept next word from suggestion",
+      },
+      {
+        "<A-l>",
+        function()
+          require("neocodeium").accept_line()
+        end,
+        mode = { "i" },
+        desc = "Accept line from suggestion",
+      },
+      {
+        "<A-e>",
+        function()
+          require("neocodeium").cycle_or_complete()
+        end,
+        mode = { "i" },
+        desc = "Cycle or complete forwad",
+      },
+      {
+        "<A-r>",
+        function()
+          require("necodeium").cycle_or_complete(-1)
+        end,
+        mode = { "i" },
+        desc = "Cycle or complete backwards",
+      },
+      {
+        "<A-c>",
+        function()
+          require("neocodeium").clear()
+        end,
+        mode = { "i" },
+        desc = "Clear suggestions",
+      },
     },
   },
 }
