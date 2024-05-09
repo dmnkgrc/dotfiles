e2f529f34e2444cc9e54fffff66dfc4059a87476
Dominik Garcia Bertapelle <d.bertapelle@hellogetsafe.com>
2024-05-09 23:55:23 +0200

    chore: many changes

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -123,4 +123,12 @@ return {
       },
     },
   },
+  {
+    "otavioschwanck/arrow.nvim",
+    opts = {
+      show_icons = true,
+      leader_key = ";",
+      buffer_leader_key = "m",
+    },
+  },
 }
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -8,20 +8,69 @@ return {
     enabled = false,
   },
   {
-    "letieu/harpoon-lualine",
-    dependencies = {
-      {
-        "ThePrimeagen/harpoon",
-        branch = "harpoon2",
-      },
-    },
-  },
-  {
     "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "meuter/lualine-so-fancy.nvim",
+    },
     opts = {
+      options = {
+        theme = "auto",
+        component_separators = { left = "│", right = "│" },
+        section_separators = { left = "", right = "" },
+        globalstatus = true,
+        icons_enabled = true,
+        refresh = {
+          statusline = 100,
+        },
+      },
       sections = {
-        lualine_c = { "harpoon2" },
+        lualine_a = {
+          {},
+        },
+        lualine_b = {
+          { "fancy_branch" },
+        },
+        lualine_c = {
+          {
+            "filename",
+            path = 1, -- 2 for full path
+            symbols = {
+              modified = "  ",
+              -- readonly = "  ",
+              -- unnamed = "  ",
+            },
+          },
+          { "fancy_diagnostics", sources = { "nvim_lsp" }, symbols = { error = " ", warn = " ", info = " " } },
+          { "fancy_searchcount" },
+        },
+        lualine_x = {
+          { "fancy_lsp_servers" },
+          { "fancy_diff" },
+          { "progress" },
+        },
+        lualine_y = {},
+        lualine_z = {
+          { "fancy_macro" },
+        },
       },
     },
   },
+  -- {
+  --   "letieu/harpoon-lualine",
+  --   dependencies = {
+  --     {
+  --       "ThePrimeagen/harpoon",
+  --       branch = "harpoon2",
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   opts = {
+  --     sections = {
+  --       lualine_c = { "harpoon2" },
+  --     },
+  --   },
+  -- },
 }
