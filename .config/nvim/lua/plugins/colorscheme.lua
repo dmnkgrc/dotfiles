d8c7d515d0ea151cbf3cbb24d2e020b977be76b1
Dominik Garcia Bertapelle <d.bertapelle@hellogetsafe.com>
2024-05-14 23:11:56 +0200

    style: new and better design

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -9,6 +9,25 @@ return {
     end,
   },
   {
+    'dgox16/oldworld.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      local p = require 'oldworld.palette'
+      local colorbuddy = require 'colorbuddy'
+      local Color = colorbuddy.Color
+      local colors = colorbuddy.colors
+      local styles = colorbuddy.styles
+      local Group = colorbuddy.Group
+      require('oldworld').setup()
+      vim.cmd.colorscheme 'oldworld'
+      Color.new('lavender', p.lavender)
+
+      Group.new('MiniJump2dSpot', colors.gray1, colors.lavender, styles.bold)
+      Group.new('FzfLuaBorder', colors.bg)
+    end,
+  },
+  {
     'jesseleite/nvim-noirbuddy',
     name = 'noirbuddy',
     dependencies = {
@@ -16,56 +35,56 @@ return {
     },
     lazy = false,
     priority = 1000,
-    config = function()
-      local colorbuddy = require 'colorbuddy'
-      local Color = colorbuddy.Color
-      local colors = colorbuddy.colors
-      local Group = colorbuddy.Group
-
-      require('noirbuddy').setup {
-        colors = {
-          background = '#1E1E1E',
-          primary = '#967bb6',
-          secondary = '#CACCCA',
-          colors = {
-            noir_0 = '#ffffff', -- `noir_0` is light for dark themes, and dark for light themes
-            noir_1 = '#f5f5f5',
-            noir_2 = '#d5d5d5',
-            noir_3 = '#b4b4b4',
-            noir_4 = '#a7a7a7',
-            noir_5 = '#949494',
-            noir_6 = '#737373',
-            noir_7 = '#535353',
-            noir_8 = '#323232',
-            noir_9 = '#212121', -- `noir_9` is dark for dark themes, and light for light themes
-            diagnostic_error = '#D16969',
-            diagnostic_warning = '#D7BA7D',
-            diagnostic_info = '#bdacd1',
-            diagnostic_hint = '#969696',
-            diff_add = '#69AD7A',
-            diff_change = '#D7BA7D',
-            diff_delete = '#D16969',
-          },
-        },
-      }
-      Color.new('accent', '#9bb67b')
-      Color.new('green', '#9bb67b')
-      Color.new('string', '#bdacd1')
-      Group.new('FlashLabel', colors.string)
-      Group.new('FlashBackdrop', colors.noir_7)
-      Group.new('FlashCursor', colors.secondary)
-      Group.new('NoiceCursor', colors.primary)
-      Group.new('Identifier', colors.secondary)
-      Group.new('@identifier', colors.secondary)
-      Group.new('@lsp.type.function', colors.accent)
-      Group.new('@lsp.type.method', colors.accent)
-      Group.new('Function', colors.accent)
-      Group.new('@function', colors.accent)
-      Group.new('@function.builtin', colors.accent)
-      Group.new('@function.call', colors.accent)
-      Group.new('@constructor', colors.accent)
-      Group.new('@comment', colors.accent)
-      Group.new('@string', colors.string)
-    end,
+    -- config = function()
+    --   local colorbuddy = require 'colorbuddy'
+    --   local Color = colorbuddy.Color
+    --   local colors = colorbuddy.colors
+    --   local Group = colorbuddy.Group
+    --
+    --   require('noirbuddy').setup {
+    --     colors = {
+    --       background = '#1E1E1E',
+    --       primary = '#967bb6',
+    --       secondary = '#CACCCA',
+    --       colors = {
+    --         noir_0 = '#ffffff', -- `noir_0` is light for dark themes, and dark for light themes
+    --         noir_1 = '#f5f5f5',
+    --         noir_2 = '#d5d5d5',
+    --         noir_3 = '#b4b4b4',
+    --         noir_4 = '#a7a7a7',
+    --         noir_5 = '#949494',
+    --         noir_6 = '#737373',
+    --         noir_7 = '#535353',
+    --         noir_8 = '#323232',
+    --         noir_9 = '#212121', -- `noir_9` is dark for dark themes, and light for light themes
+    --         diagnostic_error = '#D16969',
+    --         diagnostic_warning = '#D7BA7D',
+    --         diagnostic_info = '#bdacd1',
+    --         diagnostic_hint = '#969696',
+    --         diff_add = '#69AD7A',
+    --         diff_change = '#D7BA7D',
+    --         diff_delete = '#D16969',
+    --       },
+    --     },
+    --   }
+    --   Color.new('accent', '#9bb67b')
+    --   Color.new('green', '#9bb67b')
+    --   Color.new('string', '#bdacd1')
+    --   Group.new('FlashLabel', colors.string)
+    --   Group.new('FlashBackdrop', colors.noir_7)
+    --   Group.new('FlashCursor', colors.secondary)
+    --   Group.new('NoiceCursor', colors.primary)
+    --   Group.new('Identifier', colors.secondary)
+    --   Group.new('@identifier', colors.secondary)
+    --   Group.new('@lsp.type.function', colors.accent)
+    --   Group.new('@lsp.type.method', colors.accent)
+    --   Group.new('Function', colors.accent)
+    --   Group.new('@function', colors.accent)
+    --   Group.new('@function.builtin', colors.accent)
+    --   Group.new('@function.call', colors.accent)
+    --   Group.new('@constructor', colors.accent)
+    --   Group.new('@comment', colors.accent)
+    --   Group.new('@string', colors.string)
+    -- end,
   },
 }
