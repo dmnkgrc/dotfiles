e7f1a70659996f9f888e4c5cd51ce39edfd82f6a
Dominik Garcia Bertapelle <d.bertapelle@hellogetsafe.com>
2024-05-19 15:25:57 +0200

    refactor: split into more atomic files

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -1,3 +1,10 @@
 return {
   { 'mistricky/codesnap.nvim', build = 'make', even = 'VeryLazy' },
+  {
+    'max397574/better-escape.nvim',
+    config = true,
+    opts = {
+      mapping = { 'jk', 'jj' },
+    },
+  },
 }
