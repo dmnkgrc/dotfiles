4581b27a41ffd344333b0c1753fef00b4afd857a
Dominik Garcia Bertapelle <d.bertapelle@hellogetsafe.com>
2024-05-19 15:15:51 +0200

    chore: split into more atomic files

---
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    'altermo/ultimate-autopair.nvim',
+    event = { 'InsertEnter', 'CmdlineEnter' },
+    branch = 'v0.6',
+    opts = {
+      tabout = {
+        enable = true,
+        hopout = true,
+      },
+    },
+  },
+}
