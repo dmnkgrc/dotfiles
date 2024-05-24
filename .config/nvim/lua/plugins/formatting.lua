e972ed0a69eab8e13cb2186df8f74f60819234f4
Dominik Garcia Bertapelle <d.bertapelle@hellogetsafe.com>
2024-05-24 21:06:33 +0200

    feat: use own formatter to pickup biome or prettier

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -1,32 +1,45 @@
 return {
   {
     'stevearc/conform.nvim',
-    opts = {
-      format_on_save = {
-        timeout_ms = 3000,
-        lsp_fallback = true,
-      },
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        fish = { 'fish_indent' },
-        sh = { 'shfmt' },
-        ['javascript'] = { { 'biome', 'prettier' } },
-        ['javascriptreact'] = { { 'biome', 'prettier' } },
-        ['typescript'] = { { 'biome', 'prettier' } },
-        ['typescriptreact'] = { { 'biome', 'prettier' } },
-        ['vue'] = { { 'biome', 'prettier' } },
-        ['css'] = { { 'biome', 'prettier' } },
-        ['scss'] = { { 'biome', 'prettier' } },
-        ['less'] = { { 'biome', 'prettier' } },
-        ['html'] = { { 'biome', 'prettier' } },
-        ['json'] = { { 'biome', 'prettier' } },
-        ['jsonc'] = { { 'biome', 'prettier' } },
-        ['yaml'] = { { 'biome', 'prettier' } },
-        ['markdown'] = { { 'biome', 'prettier' } },
-        ['markdown.mdx'] = { { 'biome', 'prettier' } },
-        ['graphql'] = { { 'biome', 'prettier' } },
-        ['handlebars'] = { { 'biome', 'prettier' } },
-      },
-    },
+    config = function()
+      local conform = require 'conform'
+      conform.setup {
+        format_on_save = {
+          timeout_ms = 3000,
+          lsp_fallback = true,
+        },
+        formatters = {
+          biome_or_prettier = {
+            command = 'biome_or_prettier',
+            args = { '--path', '$FILENAME' },
+            stdin = true,
+
+            cwd = require('conform.util').root_file { 'package.json' },
+          },
+        },
+        formatters_by_ft = {
+          lua = { 'stylua' },
+          fish = { 'fish_indent' },
+          sh = { 'shfmt' },
+          rust = { 'rustfmt' },
+          ['javascript'] = { { 'biome_or_prettier' } },
+          ['javascriptreact'] = { { 'biome_or_prettier' } },
+          ['typescript'] = { { 'biome_or_prettier' } },
+          ['typescriptreact'] = { { 'biome_or_prettier' } },
+          ['vue'] = { { 'biome_or_prettier' } },
+          ['css'] = { { 'biome_or_prettier' } },
+          ['scss'] = { { 'biome_or_prettier' } },
+          ['less'] = { { 'biome_or_prettier' } },
+          ['html'] = { { 'biome_or_prettier' } },
+          ['json'] = { { 'biome_or_prettier' } },
+          ['jsonc'] = { { 'biome_or_prettier' } },
+          ['yaml'] = { { 'biome_or_prettier' } },
+          ['markdown'] = { { 'biome_or_prettier' } },
+          ['markdown.mdx'] = { { 'biome_or_prettier' } },
+          ['graphql'] = { { 'biome_or_prettier' } },
+          ['handlebars'] = { { 'biome_or_prettier' } },
+        },
+      }
+    end,
   },
 }
