0e8579a287737c232560ed6b437b91e961889ee7
Dominik Garcia Bertapelle <d.bertapelle@hellogetsafe.com>
2024-05-24 21:05:00 +0200

    feat: go back to wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,15 +2,9 @@
 local wezterm = require("wezterm")
 local mux = wezterm.mux
 
-local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm/")
-
 -- This table will hold the configuration.
 local config = {
 	-- window_background_opacity = 0.95,
-	window_padding = {
-		left = 0,
-		right = 0,
-	},
 	hide_tab_bar_if_only_one_tab = true,
 	hide_mouse_cursor_when_typing = false,
 	inactive_pane_hsb = {
@@ -22,38 +16,31 @@ local config = {
 	enable_scroll_bar = true,
 
 	status_update_interval = 1000,
-	-- font = wezterm.font("Berkeley Mono Variable"),
 	font = wezterm.font("MonoLisa"),
-	font_size = 14,
+	font_size = 15,
 }
 
--- Leader is the same as my old tmux prefix
 config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
 
 config.keys = require("keybinds")
 
-for _, value in ipairs(require("plugins.smart-splits").keys) do
-	table.insert(config.keys, value)
-end
+-- for _, value in ipairs(require("plugins.smart-splits").keys) do
+-- 	table.insert(config.keys, value)
+-- end
 
-local function mergeTables(t1, t2)
-	for key, value in pairs(t2) do
-		t1[key] = value
-	end
-end
+-- local function mergeTables(t1, t2)
+-- 	for key, value in pairs(t2) do
+-- 		t1[key] = value
+-- 	end
+-- end
 
-local colors = require("colors")
-mergeTables(config, colors)
+config.color_scheme = "rose-pine-moon"
+-- local colors = require("colors")
+-- mergeTables(config, colors)
 
 wezterm.on("gui-startup", function(cmd)
 	local _, _, window = mux.spawn_window(cmd or {})
 	window:gui_window():maximize()
-end)
-
-workspace_switcher.apply_to_config(config, "s", "LEADER", function(label)
-	return wezterm.format({
-		{ Text = "󱂬: " .. label },
-	})
 end)
 
 -- and finally, return the configuration to wezterm
