1979d377e504cce19e733898e8aad6de806ee4e1
Dominik Garcia Bertapelle <d.bertapelle@hellogetsafe.com>
2024-05-24 21:05:48 +0200

    chore: remove unnused function

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -32,10 +32,6 @@ return {
           --  Similar to document symbols, except searches over your entire project.
           map('<leader>ws', require('fzf-lua').lsp_live_workspace_symbols, '[W]orkspace [S]ymbols')
 
-          -- Rename the variable under your cursor.
-          --  Most Language Servers support renaming across files, etc.
-          map('<leader>cr', vim.lsp.buf.rename, '[R]e[n]ame')
-
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
