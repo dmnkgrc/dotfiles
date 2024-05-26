d838a47e4193fb0e5ee2ee8de7ae791dd0b7ea1c
Dominik Garcia Bertapelle <d.bertapelle@hellogetsafe.com>
2024-05-26 21:30:58 +0200

    chore: trying to get better at nvmim

---
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    'm4xshen/hardtime.nvim',
+    event = 'BufEnter',
+    dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+    opts = {},
+  },
+  {
+    'tris203/precognition.nvim',
+    event = 'BufEnter',
+    opts = {},
+  },
+}
