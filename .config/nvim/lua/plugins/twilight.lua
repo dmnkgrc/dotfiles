229dd9c6b06d51c9e20ee79766a3a777c957e5e7
Dominik Garcia Bertapelle <d.bertapelle@hellogetsafe.com>
2024-08-29 10:05:42 +0200

    chore: config updates

--- .config/nvim/lua/plugins/twilight.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'folke/twilight.nvim',
-  opts = {},
-  keys = {
-    { '<leader>ct', '<cmd>Twilight<cr>', desc = 'Toggle Twilight' },
-  },
-}
--- .config/nvim/lua/plugins/typescript.lua
+++ .config/nvim/lua/plugins/typescript.lua
@@ -8,17 +8,13 @@ return {
     'pmizio/typescript-tools.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
     opts = {
-      expose_as_code_action = {
-        'add_missing_imports',
-        'remove_unused',
-        'remove_unused_imports',
-      },
+      expose_as_code_action = 'all',
       settings = {
         tsserver_file_preferences = {
           includeInlayParameterNameHints = 'all',
           includeCompletionsForModuleExports = true,
           quotePreference = 'auto',
-          importModuleSpecifierPreference = 'relative',
+          importModuleSpecifierPreference = 'non-relative',
         },
       },
     },
