d208501fbec1d54862a46870089e85a55c326cee
Dominik Garcia Bertapelle <dominik.bertapelle@cortea.ai>
2025-04-15 10:50:37 +0200

    style: use night owl

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,17 @@
 return {
   {
+    "oxfist/night-owl.nvim",
+    lazy = false,         -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000,      -- make sure to load this before all the other start plugins
+    opts = {
+      transparent = true, -- enable transparent background
+    },
+  },
+  {
     "rebelot/kanagawa.nvim",
     priority = 1000,
     lazy = false,
+    enabled = false,
     opts = {
       transparent = true,
     },
@@ -10,7 +19,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "night-owl",
     },
   },
 }
