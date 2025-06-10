6e3d15907c2ac6b7a8c0158c76c24728d2891b48
Dominik Garcia Bertapelle <hello@dmnkgrc.com>
2025-06-10 15:34:34 +0200

    feat: formatting with trunk configs

--- .config/nvim/lua/plugins/linting.lua
+++ .config/nvim/lua/plugins/linting.lua
@@ -1,21 +1,39 @@
 return {
   {
     "stevearc/conform.nvim",
-    enabled = false,
+    enabled = true,
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters_by_ft.python = { "ruff_format" }
+      opts.formatters_by_ft.javascript = { "biome" }
+      opts.formatters_by_ft.typescript = { "biome" }
+      opts.formatters_by_ft.javascriptreact = { "biome" }
+      opts.formatters_by_ft.typescriptreact = { "biome" }
+      opts.formatters_by_ft.json = { "biome" }
+
+      -- Custom formatter definitions to use .trunk/configs
+      opts.formatters = opts.formatters or {}
+      opts.formatters.biome = {
+        command = "biome",
+        args = function(self, ctx)
+          local config_path = vim.fn.findfile("biome.json", ctx.dirname .. ";")
+          if config_path == "" then
+            config_path = vim.fn.findfile(".trunk/configs/biome.json", ctx.dirname .. ";")
+          end
+          if config_path ~= "" then
+            return { "format", "--stdin-file-path", "$FILENAME", "--config-path", config_path }
+          end
+          vim.notify("Biome formatter using default config", vim.log.levels.INFO)
+          return { "format", "--stdin-file-path", "$FILENAME" }
+        end,
+        stdin = true,
+      }
+
+      return opts
+    end,
   },
   {
     "trunk-io/neovim-trunk",
-    enabled = true,
-    lazy = false,
-    -- these are optional config arguments (defaults shown)
-    config = {
-      -- trunkPath = "trunk",
-      -- lspArgs = {},
-      -- formatOnSave = true,
-      -- formatOnSaveTimeout = 10, -- seconds
-      -- logLevel = "info"
-    },
-    main = "trunk",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    enabled = false,
   },
 }
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,94 +1,124 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        vtsls = {
-          settings = {
-            typescript = {
-              preferences = {
-                importModuleSpecifier = "non-relative",
+    opts = function(_, opts)
+      return vim.tbl_deep_extend("force", opts, {
+        servers = {
+          vtsls = {
+            settings = {
+              typescript = {
+                preferences = {
+                  importModuleSpecifier = "non-relative",
+                },
               },
-            },
-            javascript = {
-              preferences = {
-                importModuleSpecifier = "non-relative",
-              },
-            },
-          }
-        },
-        tailwindcss = {
-          settings = {
-            tailwindCSS = {
-              experimental = {
-                classRegex = {
-                  { "([\"'`][^\"'`]*.*?[\"'`])", "[\"'`]([^\"'`]*).*?[\"'`]" },
+              javascript = {
+                preferences = {
+                  importModuleSpecifier = "non-relative",
                 },
               },
             },
           },
-        },
-      },
-      setup = {
-        vtsls = function(_, opts)
-          local util = require("lspconfig.util")
-          opts.on_attach = function(client)
-            client.server_capabilities.documentFormattingProvider = false
-            client.server_capabilities.documentRangeFormattingProvider = false
-          end
-          opts.root_dir = function(fname)
-            return util.root_pattern(".trunk", ".git")(fname)
-          end
-          require("lspconfig").vtsls.setup(opts)
-          return true
-        end,
-        pyright = function(_, opts)
-          local util = require("lspconfig.util")
-          opts.root_dir = function(fname)
-            return util.root_pattern(".trunk", ".git")(fname)
-          end
-          opts.on_attach = function(client)
-            client.server_capabilities.documentFormattingProvider = false
-            client.server_capabilities.documentRangeFormattingProvider = false
-          end
-          require("lspconfig").pyright.setup(opts)
-          return true
-        end,
-        ruff = function(_, opts)
-          local util = require("lspconfig.util")
-          opts.root_dir = function(fname)
-            return util.root_pattern(".trunk", ".git")(fname)
-          end
-          opts.on_attach = function(client)
-            client.server_capabilities.documentFormattingProvider = false
-            client.server_capabilities.documentRangeFormattingProvider = false
-          end
-          require("lspconfig").ruff.setup(opts)
-          return true
-        end,
-        tailwindcss = function(_, opts)
-          opts.filetypes = opts.filetypes or {}
-
-          --- @param ft string
-          opts.filetypes = vim.tbl_filter(function(ft)
-            return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
-          end, opts.filetypes)
-
-          opts.settings = {
-            tailwindCSS = {
-              experimental = {
-                classRegex = {
-                  { "([\"'`][^\"'`]*.*?[\"'`])", "[\"'`]([^\"'`]*).*?[\"'`]" }
+          tailwindcss = {
+            settings = {
+              tailwindCSS = {
+                experimental = {
+                  classRegex = {
+                    { "([\"'`][^\"'`]*.*?[\"'`])", "[\"'`]([^\"'`]*).*?[\"'`]" },
+                  },
                 },
               },
             },
-          }
+          },
+          pyright = {},
+          ruff_lsp = {},
+          biome = {},
+        },
+        setup = {
+          vtsls = function(_, opts)
+            local util = require("lspconfig.util")
+            opts.on_attach = function(client)
+              client.server_capabilities.documentFormattingProvider = false
+              client.server_capabilities.documentRangeFormattingProvider = false
+            end
+            opts.root_dir = function(fname)
+              return util.root_pattern("tsconfig.json", "package.json")(fname)
+            end
+            require("lspconfig").vtsls.setup(opts)
+            return true
+          end,
+          pyright = function(_, opts)
+            local util = require("lspconfig.util")
+            opts.root_dir = function(fname)
+              return util.root_pattern("pyproject.toml", "setup.py", ".trunk", ".git")(fname)
+            end
 
-          vim.list_extend(opts.filetypes, opts.filetypes_include or {})
-          require("lspconfig").tailwindcss.setup(opts)
-          return true
-        end,
-      }
-    },
+            -- Look for pyrightconfig in .trunk/configs
+            opts.on_new_config = function(config, root_dir)
+              local pyright_config = vim.fn.findfile(".trunk/configs/pyrightconfig.json", root_dir)
+              if pyright_config ~= "" then
+                config.settings = vim.tbl_deep_extend("force", config.settings or {}, {
+                  python = {
+                    pythonPath = vim.fn.fnamemodify(pyright_config, ":h"),
+                  },
+                })
+                config.cmd_env = config.cmd_env or {}
+                config.cmd_env.PYRIGHT_CONFIG_PATH = pyright_config
+              end
+            end
+
+            require("lspconfig").pyright.setup(opts)
+            return true
+          end,
+          ruff_lsp = function(_, opts)
+            local util = require("lspconfig.util")
+            opts.root_dir = function(fname)
+              return util.root_pattern("pyproject.toml", "ruff.toml", ".trunk", ".git")(fname)
+            end
+            opts.on_attach = function(client)
+              -- Disable hover in favor of pyright
+              client.server_capabilities.hoverProvider = false
+            end
+
+            -- Look for ruff config in .trunk/configs
+            opts.on_new_config = function(config, root_dir)
+              local ruff_config = vim.fn.findfile(".trunk/configs/ruff.toml", root_dir)
+              if ruff_config ~= "" then
+                config.init_options = vim.tbl_deep_extend("force", config.init_options or {}, {
+                  settings = {
+                    configurationPreference = "filesystemFirst",
+                    configuration = ruff_config,
+                  },
+                })
+              end
+            end
+
+            require("lspconfig").ruff_lsp.setup(opts)
+            return true
+          end,
+          tailwindcss = function(_, opts)
+            opts.filetypes = opts.filetypes or {}
+
+            --- @param ft string
+            opts.filetypes = vim.tbl_filter(function(ft)
+              return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+            end, opts.filetypes)
+
+            opts.settings = {
+              tailwindCSS = {
+                experimental = {
+                  classRegex = {
+                    { "([\"'`][^\"'`]*.*?[\"'`])", "[\"'`]([^\"'`]*).*?[\"'`]" },
+                  },
+                },
+              },
+            }
+
+            vim.list_extend(opts.filetypes, opts.filetypes_include or {})
+            require("lspconfig").tailwindcss.setup(opts)
+            return true
+          end,
+        },
+      })
+    end,
   },
 }
