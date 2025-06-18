584d1a813a6e3b5dc15d0d902f850cdb7a0dceb5
Dominik Garcia Bertapelle <hello@dmnkgrc.com>
2025-06-18 11:14:42 +0200

    chore: better harpoon delete

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,85 +1,94 @@
-return {
-  {
-    'ThePrimeagen/harpoon',
-    branch = 'harpoon2',
-    opts = {
-      menu = {
-        width = vim.api.nvim_win_get_width(0) - 4,
-      },
-      settings = {
-        save_on_toggle = true,
-      },
-    },
-    keys = function()
-      local keys = {
-        {
-          '<leader>H',
-          function()
-            require('harpoon'):list():add()
-          end,
-          desc = 'Harpoon File',
-        },
-        {
-          '<leader>h',
-          function()
-            local harpoon = require 'harpoon'
-            harpoon.ui:toggle_quick_menu(harpoon:list())
-          end,
-          desc = 'Harpoon Quick Menu',
-        },
-        {
-          '<leader>fh',
-          function()
-            require('snacks').picker {
-              finder = function()
-                local harpoon = require 'harpoon'
-                local file_paths = {}
-                for _, item in ipairs(harpoon:list().items) do
-                  table.insert(file_paths, {
-                    text = item.value,
-                    file = item.value,
-                  })
-                end
-                return file_paths
-              end,
-              win = {
-                input = {
-                  keys = {
-                    ['dd'] = { 'harpoon_delete', mode = { 'n', 'x' } },
-                  },
-                },
-                list = {
-                  keys = {
-                    ['dd'] = { 'harpoon_delete', mode = { 'n', 'x' } },
-                  },
-                },
-              },
-              actions = {
-                harpoon_delete = function(picker, item)
-                  local harpoon = require 'harpoon'
-                  local to_remove = item or picker:selected()
-                  table.remove(harpoon:list().items, to_remove.idx)
-                  picker:find {
-                    refresh = true,
-                  }
-                end,
-              },
-            }
-          end,
-          desc = 'Find harpoon',
-        },
-      }
+local normalize_list = function(t)
+	local normalized = {}
+	for _, v in pairs(t) do
+		if v ~= nil then
+			table.insert(normalized, v)
+		end
+	end
+	return normalized
+end
 
-      for i = 1, 5 do
-        table.insert(keys, {
-          '<leader>' .. i,
-          function()
-            require('harpoon'):list():select(i)
-          end,
-          desc = 'Harpoon to File ' .. i,
-        })
-      end
-      return keys
-    end,
-  },
+return {
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		opts = {
+			menu = {
+				width = vim.api.nvim_win_get_width(0) - 4,
+			},
+			settings = {
+				save_on_toggle = true,
+			},
+		},
+		keys = function()
+			local keys = {
+				{
+					"<leader>H",
+					function()
+						require("harpoon"):list():add()
+					end,
+					desc = "Harpoon File",
+				},
+				{
+					"<leader>h",
+					function()
+						local harpoon = require("harpoon")
+						harpoon.ui:toggle_quick_menu(harpoon:list())
+					end,
+					desc = "Harpoon Quick Menu",
+				},
+				{
+					"<leader>fh",
+					function()
+						require("snacks").picker({
+							finder = function()
+								local harpoon = require("harpoon")
+								local file_paths = {}
+								for _, item in ipairs(harpoon:list().items) do
+									table.insert(file_paths, {
+										text = item.value,
+										file = item.value,
+									})
+								end
+								return file_paths
+							end,
+							win = {
+								input = {
+									keys = {
+										["dd"] = { "harpoon_delete", mode = { "n", "x" } },
+									},
+								},
+								list = {
+									keys = {
+										["dd"] = { "harpoon_delete", mode = { "n", "x" } },
+									},
+								},
+							},
+							actions = {
+								harpoon_delete = function(picker, item)
+									local harpoon = require("harpoon")
+									local to_remove = item or picker:selected()
+									harpoon:list():remove({ value = to_remove.text })
+									harpoon:list().items = normalize_list(harpoon:list().items)
+									picker:find({ refresh = true })
+								end,
+							},
+						})
+					end,
+					desc = "Find harpoon",
+				},
+			}
+
+			for i = 1, 5 do
+				table.insert(keys, {
+					"<leader>" .. i,
+					function()
+						require("harpoon"):list():select(i)
+					end,
+					desc = "Harpoon to File " .. i,
+				})
+			end
+			return keys
+		end,
+	},
 }
