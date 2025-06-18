144a18092beb0068a4a364f33c34f4d852dfff29
Dominik Garcia Bertapelle <hello@dmnkgrc.com>
2025-06-18 11:53:54 +0200

    chore: correctly set colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,24 @@
 return {
-  -- Nightfox colorscheme
-  {
-    'EdenEast/nightfox.nvim',
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    'datsfilipe/vesper.nvim',
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    'rebelot/kanagawa.nvim',
-    lazy = false,
-    priority = 1000,
-    opts = {
-      theme = 'dragon',
-    },
-  },
+	-- Nightfox colorscheme
+	{
+		"EdenEast/nightfox.nvim",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"datsfilipe/vesper.nvim",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			theme = "dragon",
+		},
+		config = function()
+			vim.cmd("colorscheme kanagawa")
+		end,
+	},
 }
